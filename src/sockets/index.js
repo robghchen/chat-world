3dc2e4284a39b77806390b5e90e12f70f238f1a7
rob <robertchen234@gmail.com>
2019-06-09 23:25:58 -0400

    translate to spanish for readers

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,32 +1,41 @@
-import *  as types from '../constants/ActionTypes'
-import {addUser, messageReceived, populateUsersList} from '../actions'
+import * as types from "../constants/ActionTypes";
+import { addUser, messageReceived, populateUsersList } from "../actions";
 
 const setupSocket = (dispatch, username) => {
-	const socket = new WebSocket('ws://localhost:8989')
+  const socket = new WebSocket("ws://localhost:8989");
 
-	socket.onopen = () => {
-		socket.send(JSON.stringify({
-			type: types.ADD_USER,
-			name: username
-		}))
-	}
-	socket.onmessage = (event) => {
-		const data = JSON.parse(event.data)
-		switch (data.type) {
-			case types.ADD_MESSAGE:
-				dispatch(messageReceived(data.message, data.author))
-				break
-			case types.ADD_USER:
-				dispatch(addUser(data.name))
-				break
-			case types.USERS_LIST:
-				dispatch(populateUsersList(data.users))
-				break
-			default:
-				break
-		}
-	}
-	return socket
-}
+  socket.onopen = () => {
+    socket.send(
+      JSON.stringify({
+        type: types.ADD_USER,
+        name: username
+      })
+    );
+  };
+  socket.onmessage = event => {
+    const data = JSON.parse(event.data);
+    const apiKey = process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY;
+    let googleTranslate = require("google-translate")(apiKey);
+    let msg = data.message ? data.message : "";
 
-export default setupSocket
+    switch (data.type) {
+      case types.ADD_MESSAGE:
+        googleTranslate.translate(msg, "es", function(err, translation) {
+          msg = translation.translatedText;
+          dispatch(messageReceived(msg, data.author));
+        });
+        break;
+      case types.ADD_USER:
+        dispatch(addUser(data.name));
+        break;
+      case types.USERS_LIST:
+        dispatch(populateUsersList(data.users));
+        break;
+      default:
+        break;
+    }
+  };
+  return socket;
+};
+
+export default setupSocket;
