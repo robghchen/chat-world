928a1c6e2a9f5c1453cb242705ed66f1d47eecb8
rob <robertchen234@gmail.com>
2019-06-12 17:47:13 -0400

    change text for select option prompt to user's language

--- src/components/LanguagePopup.js
+++ src/components/LanguagePopup.js
@@ -1,27 +1,59 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-import { googleTranslate } from "../utils/googleTranslate";
+import {
+  googleTranslate,
+  maybeTranslate,
+  translate
+} from "../utils/googleTranslate";
+import { cookies } from "../utils/cookies";
 
 class LanguagePopup extends Component {
-  state = { languageCodes: [] };
+  state = {
+    languageCodes: [],
+    str: "What language do you prefer to read with?"
+  };
 
   componentDidMount() {
-    const getLanguageCodes = languageCodes => {
-      this.setState({ languageCodes });
-    };
-
     googleTranslate.getSupportedLanguages("en", function(err, languageCodes) {
       getLanguageCodes(languageCodes);
     });
+
+    const getLanguageCodes = languageCodes => {
+      this.setState({ languageCodes });
+    };
+  }
+
+  componentDidUpdate() {
+    let { str } = this.state;
+
+    googleTranslate.detectLanguage(str, function(err, detection) {
+      if (detection.language !== cookies.get("language")) {
+        googleTranslate.translate(str, cookies.get("language"), function(
+          err,
+          translation
+        ) {
+          str = translation.translatedText;
+          translating(str);
+        });
+      }
+    });
+
+    const translating = str => {
+      if (str !== this.state.str) {
+        this.setState({ str });
+      }
+    };
   }
 
   render() {
-    const { languageCodes } = this.state;
+    const { languageCodes, str } = this.state;
     const { language, dispatch } = this.props;
+
     return (
       <div>
-        <p>What language do you prefer to read with?</p>
+        <p>{str}</p>
+
         <select
           className="select-language"
           value={language}
--- src/utils/googleTranslate.js
+++ src/utils/googleTranslate.js
@@ -1,3 +1,23 @@
+import { cookies } from "../utils/cookies";
+
 const apiKey = process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY;
 
 export const googleTranslate = require("google-translate")(apiKey);
+
+export const translate = str => {
+  googleTranslate.detectLanguage(str, function(err, detection) {
+    if (detection.language !== cookies.get("language")) {
+      googleTranslate.translate(str, cookies.get("language"), function(
+        err,
+        translation
+      ) {
+        str = translation.translatedText;
+      });
+    }
+    translating(str);
+  });
+
+  const translating = str => {
+    this.setState({ str });
+  };
+};
